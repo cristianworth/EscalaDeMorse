94db5ed9c4ab6d0668df177b34ee97e284211cf0
cristianworth <cristianweissmantel@gmail.com>
2020-06-02 00:02:42 -0300

    Criado tabelas no banco; CRUD de Paciente;

---
+++ ComandosBanco.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA `MorseCristian`;
+CREATE TABLE `MorseCristian`.`Paciente` (
+  `codigo` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NULL,
+  `data_nascimento` DATETIME NULL,
+  `cpf` VARCHAR(45) NULL,
+  PRIMARY KEY (`codigo`)
+);
+CREATE TABLE `MorseCristian`.`Formulario` (
+  `codigo` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(200) NULL,
+  `data` DATETIME NULL,
+  `resultado1` INT NULL,
+  `resultado2` INT NULL,
+  `resultado3` INT NULL,
+  `resultado4` INT NULL,
+  `resultado5` INT NULL,
+  `resultadofinal` INT NULL,
+  `codigo_paciente` INT NULL,
+  PRIMARY KEY (`codigo`),
+  INDEX `codigo_paciente_idx` (`codigo_paciente` ASC),
+  CONSTRAINT `codigo_paciente` FOREIGN KEY (`codigo_paciente`) REFERENCES `MorseCristian`.`Paciente` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
